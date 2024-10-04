523f01a276b7d4e2b9ad2231506f260b3f62a860
Kavinkumar P <148183771+kavin121299@users.noreply.github.com>
2024-10-04 16:02:37 +0530

    Update Ex_2.py

--- Ex_2.py
+++ Ex_2.py
@@ -1,11 +1,7 @@
 # Average of elements in the list or tuple.
-elems = []
 def Avg(*args):
-    elems = list(args)
-    Average = sum(elems) / len(elems)
+    Average = sum(args) / len(args)
     print(Average)
 
-a = 10
-b = 5
 #Problem
-Avg(a,b)
+Avg(10,12,15,75,14)
